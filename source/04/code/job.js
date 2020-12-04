558a8da484f7d198c0b8d8036777c4190fbee086
Laura Bergoens <laura.bergoens@pix.fr>
2020-12-04 10:54:25 +0100

    exercise 04

--- source/04/code/job.js
+++ source/04/code/job.js
@@ -1,11 +1,129 @@
 const _ = require('lodash');
 
-function job(rawInput) {
-    const pattern = rawInput.split('\n');
-    return 'ok';
+class Passport {
+  constructor(passportData) {
+    this.attributes = {};
+    const items = passportData.split(' ');
+    const sortedItems = _.orderBy(items);
+    for (const item of sortedItems) {
+      const itemDetails = item.split(':');
+      this.attributes[itemDetails[0]] = itemDetails[1];
+    }
+  }
+
+  isBirthYearValid() {
+    return this.attributes.byr
+        && this.attributes.byr.toString().length === 4
+        && parseInt(this.attributes.byr) <= 2002
+        && parseInt(this.attributes.byr) >= 1920;
+  }
+
+  isIssueYearValid() {
+    return this.attributes.iyr
+        && this.attributes.iyr.toString().length === 4
+        && parseInt(this.attributes.iyr) <= 2020
+        && parseInt(this.attributes.iyr) >= 2010;
+  }
+
+  isExpirationYearValid() {
+    return this.attributes.eyr
+        && this.attributes.eyr.toString().length === 4
+        && parseInt(this.attributes.eyr) <= 2030
+        && parseInt(this.attributes.eyr) >= 2020;
+  }
+
+  isHeightValid() {
+    let isValid = false;
+    if (this.attributes.hgt) {
+      const value = this.attributes.hgt;
+      if (_.includes(value, 'cm')) {
+        const heightVal = value.split('cm')[0];
+        isValid = parseInt(heightVal) <= 193
+            && parseInt(heightVal) >= 150;
+      }
+      if (_.includes(value, 'in')) {
+        const heightVal = value.split('in')[0];
+        isValid = parseInt(heightVal) <= 76
+                  && parseInt(heightVal) >= 59;
+      }
+    }
+    return isValid;
+  }
+
+  isHairColorValid() {
+    let isValid = false;
+    if (this.attributes.hcl) {
+      const regex = /^#[a-f0-9]{6}$/g;
+      isValid = Boolean(this.attributes.hcl.match(regex));
+    }
+    return isValid;
+  }
+
+  isEyeColorValid() {
+    let isValid = false;
+    if (this.attributes.ecl) {
+      isValid = ['amb', 'blu', 'brn' ,'gry', 'grn', 'hzl', 'oth'].includes(this.attributes.ecl);
+    }
+    return isValid;
+  }
+
+  isPassportIdValid() {
+    let isValid = false;
+    if (this.attributes.pid) {
+      const regex = /^[0-9]{9}$/g;
+      isValid = Boolean(this.attributes.pid.match(regex));
+    }
+    return isValid;
+  }
+
+  isValidLight() {
+    return 'byr' in this.attributes
+    && 'iyr' in this.attributes
+    && 'eyr' in this.attributes
+    && 'hgt' in this.attributes
+    && 'hcl' in this.attributes
+    && 'ecl' in this.attributes
+    && 'pid' in this.attributes;
+  }
+
+  isValidStrong() {
+    return this.isBirthYearValid() && this.isIssueYearValid() && this.isExpirationYearValid()
+            && this.isHeightValid() && this.isHairColorValid() && this.isEyeColorValid() && this.isPassportIdValid();
+  }
+}
+
+function _preparePassports(rawInput) {
+  const passportsRaw = rawInput.split('\n\n');
+  return _.map(passportsRaw, (passportRaw) => {
+    let passportData = passportRaw;
+    passportData = passportData.replace('\n', ' '); // my replace would not work for all occurrences....
+    passportData = passportData.replace('\n', ' ');
+    passportData = passportData.replace('\n', ' ');
+    passportData = passportData.replace('\n', ' ');
+    passportData = passportData.replace('\n', ' ');
+    passportData = passportData.replace('\n', ' ');
+    passportData = passportData.replace('\n', ' ');
+    passportData = passportData.replace('\n', ' ');
+    passportData = passportData.replace('\n', ' ');
+    passportData = passportData.replace('\n', ' ');
+    return new Passport(passportData);
+  });
+}
+
+function jobLight(rawInput) {
+  const passports = _preparePassports(rawInput);
+  const validPassports = _.filter(passports, (passport) => passport.isValidLight());
+  return validPassports.length;
+}
+
+function jobStrong(rawInput) {
+  const passports = _preparePassports(rawInput);
+  const validPassports = _.filter(passports, (passport) => passport.isValidStrong());
+  return validPassports.length;
 }
 
 module.exports = {
-    job,
+  jobLight,
+  jobStrong,
 };
 
--- test/test_04.js
+++ test/test_04.js
@@ -2,19 +2,54 @@ const chai = require('chai');
 const expect = chai.expect;
 chai.use(require('chai-as-promised'));
 chai.use(require('chai-sorted'));
-const { job } = require('../source/04/code/job');
+const { jobLight, jobStrong } = require('../source/04/code/job');
 
 describe('04', () => {
-    describe('job', () => {
-        it('should return ok', () => {
+    describe('jobLight', () => {
+        it('should return valid password counts with weak checks', () => {
             // given
-            const rawInput = 'raw input';
+            const rawInput = 'ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\n' +
+                'byr:1937 iyr:2017 cid:147 hgt:183cm\n' +
+                '\n' +
+                'iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884\n' +
+                'hcl:#cfa07d byr:1929\n' +
+                '\n' +
+                'hcl:#ae17e1 iyr:2013\n' +
+                'eyr:2024\n' +
+                'ecl:brn pid:760753108 byr:1931\n' +
+                'hgt:179cm\n' +
+                '\n' +
+                'hcl:#cfa07d eyr:2025 pid:166559648\n' +
+                'iyr:2011 ecl:brn hgt:59in';
 
             // when
-            const result = job(rawInput);
+            const result = jobLight(rawInput);
 
             // then
-            expect(result).to.equal('ok');
+            expect(result).to.equal(2);
+        });
+    });
+    describe('jobStrong', () => {
+        it('should return valid password counts with strong checks', () => {
+            // given
+            const rawInput = 'pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980\n' +
+                'hcl:#623a2f\n' +
+                '\n' +
+                'eyr:2029 ecl:blu cid:129 byr:1989\n' +
+                'iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm\n' +
+                '\n' +
+                'hcl:#888785\n' +
+                'hgt:164cm byr:2001 iyr:2015 cid:88\n' +
+                'pid:545766238 ecl:hzl\n' +
+                'eyr:2022\n' +
+                '\n' +
+                'iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719';
+
+            // when
+            const result = jobStrong(rawInput);
+
+            // then
+            expect(result).to.equal(4);
         });
     });
 });
