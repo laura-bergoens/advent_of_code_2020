3f43b42150c0b673f7caa1be904254a1236b7b19
Laura Bergoens <laura.bergoens@pix.fr>
2020-12-05 10:52:31 +0100

    exercise 05

--- source/05/code/job.js
+++ source/05/code/job.js
@@ -1,10 +1,75 @@
 const _ = require('lodash');
 
-function job(_rawInput) {
-    return 'ok';
+class Seat {
+  constructor({ binaryPass }) {
+    this.binaryPass = binaryPass;
+    this.row = -1;
+    this.column = -1;
+    this.seatId =  this._computeSeatId();
+  }
+
+  _parseRow() {
+    const rowInfo = this.binaryPass.substring(0, 7);
+    const maxDigit = 6;
+    let ongoingRowCalculation = 0;
+    for (const [key, value] of Object.entries(rowInfo)) {
+      const currentMask = Math.pow(2, maxDigit - key);
+      if (value === 'B') {
+        ongoingRowCalculation = ongoingRowCalculation + currentMask;
+      }
+    }
+
+    return ongoingRowCalculation;
+  }
+
+  _parseColumn() {
+    const columnInfo = this.binaryPass.substring(7);
+    const maxDigit = 2;
+    let ongoingColumnCalculation = 0;
+    for (const [key, value] of Object.entries(columnInfo)) {
+      const currentMask = Math.pow(2, maxDigit - key);
+      if (value === 'R') {
+        ongoingColumnCalculation = ongoingColumnCalculation + currentMask;
+      }
+    }
+
+    return ongoingColumnCalculation;
+  }
+
+  _computeSeatId() {
+    this.row = this._parseRow();
+    this.column = this._parseColumn();
+    return (8 * this.row) + this.column;
+  }
+}
+
+function jobGetHigher(rawInput) {
+  const seatsData = rawInput.split('\n');
+  const seats = _.map(seatsData, (binaryPass) => new Seat({ binaryPass }));
+  if (_.isEmpty(seats)) return null;
+  return _.orderBy(seats, ['seatId'], ['desc'])[0].seatId;
+}
+
+function jobFindAvailable(rawInput) {
+  let seatsData = rawInput.split('\n');
+  seatsData = _.filter(seatsData, (seatData) => seatData.length > 0);
+  const seats = _.map(seatsData, (binaryPass) => new Seat({ binaryPass }));
+  if (_.isEmpty(seats)) return null;
+  const seatIdsAsc = _.orderBy(seats, ['seatId'], ['asc']);
+  const seatIdsDesc = _.orderBy(seats, ['seatId'], ['desc']);
+  const lowerSeatId = seatIdsAsc[0].seatId;
+  const higherSeatId = seatIdsDesc[0].seatId;
+  for (let i = lowerSeatId + 1; i < higherSeatId; ++i) {
+    const isTaken = _.find(seatIdsAsc, (seat) => seat.seatId === i);
+    if (!isTaken) {
+      return i;
+    }
+  }
+  return null;
 }
 
 module.exports = {
-    job,
+  jobGetHigher,
+  jobFindAvailable,
 };
 
--- test/test_05.js
+++ test/test_05.js
@@ -1,17 +1,37 @@
+const fs = require('fs');
 const chai = require('chai');
 const expect = chai.expect;
 chai.use(require('chai-as-promised'));
 chai.use(require('chai-sorted'));
-const { job } = require('../source/05/code/job');
+const { jobGetHigher, jobFindAvailable } = require('../source/05/code/job');
 
 describe('05', () => {
-    describe('job', () => {
-        it('should return ok', () => {
+    describe('jobGetHigher', () => {
+        it('should return the highest boarding seat id', () => {
+            // given
+            const rawInput = 'FBFBBFFRLR\n' +
+                'BFFFBBFRRR\n' +
+                'FFFBBBFRRR\n' +
+                'BBFFBBFRLL'
             // when
-            const result = job();
+            const result = jobGetHigher(rawInput);
 
             // then
-            expect(result).to.equal('ok');
+            expect(result).to.equal(820);
+        });
+    });
+
+    describe('jobFindAvailable', () => {
+        it('should return the available boarding seat id', () => {
+            // given
+            const INPUT = `${__dirname}/../source/05/input/input.txt`;
+            const rawInput = fs.readFileSync(INPUT, 'utf8');
+
+            // when
+            const result = jobFindAvailable(rawInput);
+
+            // then
+            expect(result).to.equal(739);
         });
     });
 });
