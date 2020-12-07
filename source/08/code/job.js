c643d67177e45bae05b02365dec723613b718fc7
Laura Bergoens <laura.bergoens@pix.fr>
2020-12-07 10:41:27 +0100

    prepare exercise 08

---
+++ source/08/code/job.js
@@ -0,0 +1,5 @@
+module.exports = {
+  job() {
+    return 'ok';
+  },
+};
---
+++ source/08/code/main.js
@@ -0,0 +1,28 @@
+// const fs = require('fs');
+const { job } = require('./job');
+// const INPUT = `${__dirname}/../input/input.txt`;
+
+/**
+DESCRIPTION
+ **/
+
+async function main() {
+  try {
+    // const rawInput = fs.readFileSync(INPUT, 'utf8');
+    const solution1 = job();
+    console.log(`solution1: ${solution1}`);
+  } catch (error) {
+    console.error('\n', error);
+    process.exit(1);
+  }
+}
+
+if (require.main === module) {
+  main().then(
+    () => process.exit(0),
+    (err) => {
+      console.error(err);
+      process.exit(1);
+    },
+  );
+}
